fc583f450a1d9aa9329cd44c02354eadcf970246
kingsmad <grinchcoder@gmail.com>
2017-12-28 05:40:45 +0000

    20171228s1

--- 106.construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 106.construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <map>
 #include <string>
 #include <vector>
@@ -17,6 +18,22 @@ struct TreeNode {
 class Solution {
  public:
   TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
-    
+    int n = inorder.size();
+    return calc(0, n, 0, n, inorder, postorder);
+  }
+
+  TreeNode* calc(int st1, int ed1, int st2, int ed2, const vector<int>& inorder,
+                 const vector<int>& postorder) {
+    if (st1 == ed1 || st2 == ed2) return nullptr;
+    int v = postorder.at(ed2-1);
+    TreeNode* root = new TreeNode(v);
+    //int p = lower_bound(inorder.begin() + st1, inorder.begin() + ed1, v) - 
+     //                   inorder.begin();
+    int p = st1;
+    while(p < ed1 && inorder.at(p) != v) ++p;
+    assert(inorder.at(p) == v);
+    root->left = calc(st1, p, st2, st2+p-st1, inorder, postorder);
+    root->right = calc(p+1, ed1, ed2-1-(ed1-p-1), ed2-1, inorder, postorder);
+    return root;
   }
 };
---
+++ 744.network-delay-time.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+const int maxn = 110;
+
+struct Edge {
+  int u, v, c;
+};
+
+vector<Edge> edges;
+vector<int> G[maxn];
+
+int AddEdge(int u, int v) {
+
+}
+
+class Solution {
+ public:
+  int networkDelayTime(vector<vector<int>>& times, int N, int K) {
+
+  }
+};
